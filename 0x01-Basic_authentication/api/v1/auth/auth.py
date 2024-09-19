0a6682380339ec6c1d33aa83b0f6907a77ef6253
Lucas Sekwati <Luxturesekwati@gmail.com>
2024-09-19 20:47:16 +0000

    update

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-Auth class for managing the API authentication.
+Module auth.py
+This module contains the Auth class to manage API authentication.
 """
 
 from flask import request
@@ -9,25 +10,29 @@ from typing import List, TypeVar
 class Auth:
     """
     A class to manage the API authentication.
-    """
 
+    Methods:
+    require_auth(path: str, excluded_paths: List[str]) -> bool:
+        Checks if authentication is required for a given path.
+    authorization_header(request=None) -> str:
+        Retrieves the authorization header from the request.
+    current_user(request=None) -> TypeVar('User'):
+        Retrieves the current user.
+    """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """
         Determines if authentication is required for a given path.
-        For now, always returns False.
         """
         return False
 
     def authorization_header(self, request=None) -> str:
         """
-        Retrieves the authorization header from the request.
-        For now, always returns None.
+        Retrieves the authorization header from the Flask request object.
         """
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
         """
-        Retrieves the current user.
-        For now, always returns None.
+        Retrieves the current user from the request.
         """
         return None
