76193f60c955bd6ee76b46442345d7b2527bd02d
Lucas Sekwati <Luxturesekwati@gmail.com>
2024-09-20 01:20:35 +0000

    Basic auth

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+BasicAuth class for managing basic authentication.
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """
+    A class to manage basic authentication.
+    Inherits from Auth.
+    """
+    pass
