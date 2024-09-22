4c0183f197c5cc271b7ec62f2970d437710360b2
Lucas Sekwati <Luxturesekwati@gmail.com>
2024-09-22 02:42:06 +0200

    Basic authentication

---
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+"""
+Auth class for managing the API authentication.
+"""
+
+from flask import request
+from typing import List, TypeVar
+
+
+class Auth:
+    """
+    A class to manage the API authentication.
+    """
+
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """
+        Determines if a given path requires authentication.
+
+        Args:
+            path (str): The request path.
+            excluded_paths (List[str]): A list of paths that do not require
+            authentication.
+
+        Returns:
+            bool: True if authentication is required, False otherwise.
+        """
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if not path.endswith('/'):
+            path += '/'
+
+        for excluded_path in excluded_paths:
+            # Handle wildcards: if the excluded path ends with "*"
+            if excluded_path.endswith('*'):
+                if path.startswith(excluded_path[:-1]):
+                    return False
+            elif path == excluded_path:
+                return False
+
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """
+        Retrieves the authorization header from the request.
+
+        Parameters:
+        request: The Flask request object.
+
+        Returns:
+        str: The value of the Authorization header, or None if not present.
+        """
+        if request is None:
+            return None
+        return request.headers.get('Authorization', None)
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Retrieves the current user from the request.
+
+        Parameters:
+        request: The Flask request object.
+
+        Returns:
+        TypeVar('User'): Always None for now.
+        """
+        return None
