4c85104bb47847fea7134e2559763ecea8b37050
Lucas Sekwati <Luxturesekwati@gmail.com>
2024-09-26 20:22:10 +0000

    update

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,14 +5,8 @@ DB module to manage interactions with the database.
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.exc import InvalidRequestError
-from sqlalchemy.exc import NoResultFound, InvalidRequestError
-from typing import TypeVar
+from sqlalchemy.exc import NoResultFound
 from user import Base, User
-
-
-DATA = ['id', 'email', 'hashed_password', 'session_id', 'reset_token']
 
 
 class DB:
@@ -34,7 +28,8 @@ class DB:
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a new user to the database.
+        """
+        Add a new user to the database.
 
         Args:
             email (str): The email of the user.
@@ -50,24 +45,19 @@ class DB:
 
     def find_user_by(self, **kwargs) -> User:
         """
-        Locate a user in the database by specific criteria.
+        Find a user by arbitrary keyword arguments in the database.
 
         Args:
-            **kwargs: Arbitrary keyword arguments representing
-            the search filters.
+            **kwargs: Arbitrary keyword arguments used to filter the user
+            (e.g., email, id).
 
         Returns:
-            User: The user object that matches the given criteria.
+            User: The user object that matches the given filters.
 
         Raises:
-            NoResultFound: If no user is found with the given filters.
-            InvalidRequestError: If the query parameters are invalid.
+            NoResultFound: If no user is found that matches the given filters
         """
-        try:
-            user = self._session.query(User).filter_by(**kwargs).first()
-            if not user:
-                raise NoResultFound
-            return user
-        except AttributeError as e:
-            # Catch invalid attribute filters
-            raise InvalidRequestError from e
+        user = self._session.query(User).filter_by(**kwargs).first()
+        if not user:
+            raise NoResultFound
+        return user
