833902a2acdc4984606e621e72fd3a3fd40f3dba
Lucas Sekwati <Luxturesekwati@gmail.com>
2024-09-26 19:57:05 +0000

    find user

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,14 @@ DB module to manage interactions with the database.
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
+from typing import TypeVar
 from user import Base, User
+
+
+DATA = ['id', 'email', 'hashed_password', 'session_id', 'reset_token']
 
 
 class DB:
@@ -40,3 +47,27 @@ class DB:
         self._session.add(new_user)
         self._session.commit()
         return new_user
+
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Locate a user in the database by specific criteria.
+
+        Args:
+            **kwargs: Arbitrary keyword arguments representing
+            the search filters.
+
+        Returns:
+            User: The user object that matches the given criteria.
+
+        Raises:
+            NoResultFound: If no user is found with the given filters.
+            InvalidRequestError: If the query parameters are invalid.
+        """
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
+            if not user:
+                raise NoResultFound
+            return user
+        except AttributeError as e:
+            # Catch invalid attribute filters
+            raise InvalidRequestError from e
